ab7e0b06efac969f817c75263d97cb324a85d763
daveyx <d.rehle@gmail.com>
2017-03-28 03:56:02 +0200

    project setup

---
+++ dev/js/components/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+import React from 'react';
+
+export default class App extends React.Component {
+  render() {
+    return(
+      <div>
+        <h1>Free Code Camp - Use the Twitchtv JSON API</h1>
+      </div>
+    );
+  }
+}
