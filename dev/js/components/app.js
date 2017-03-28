2967597f5b1e4ded20b6e0cb01b7ac88991f1e2f
daveyx <d.rehle@gmail.com>
2017-03-29 01:12:18 +0200

    a bit layout

--- dev/js/components/app.js
+++ dev/js/components/app.js
@@ -1,13 +1,34 @@
 'use strict';
 
 import React from 'react';
+import {Grid, Row, Col, Tabs, Tab} from 'react-bootstrap';
 import '../../css/style.css';
 
 export default class App extends React.Component {
+
+  componentDidMount() {
+    console.log("---> did mount");
+  }
+
   render() {
     return(
       <div>
-        <h1>Free Code Camp - Use the Twitchtv JSON API</h1>
+        <Grid className="content">
+          <Row>
+            <Col xs={6} xsOffset={3}>
+              <h1>Free Code Camp - Use the Twitchtv JSON API</h1>
+            </Col>
+          </Row>
+          <Row>
+            <Col xs={6} xsOffset={3}>
+              <Tabs defaultActiveKey={1} id="tabs">
+                <Tab eventKey={1} title="All">Tab 1 content</Tab>
+                <Tab eventKey={2} title="Online">Tab 2 content</Tab>
+                <Tab eventKey={3} title="Offline">Tab 3 content</Tab>
+              </Tabs>
+            </Col>
+            </Row>
+        </Grid>
       </div>
     );
   }
